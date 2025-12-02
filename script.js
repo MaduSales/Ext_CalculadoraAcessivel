2c307912524a5ca058fc227ff8340f905ae473c0
Maria Eduarda <maria.mirandadev@gmail.com>
2025-12-02 10:14:13 -0300

    UPDATE: Codificando backend da extensão no JS e aplicando modo escuro

---
+++ script.js
@@ -0,0 +1,61 @@
+const themeBtn = document.getElementById("themeIcon");
+
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+
+
+const inputText = document.getElementById("input-text");
+const resultText = document.getElementById("result");
+const numberButtons = document.querySelectorAll(".numbers-grid button:not(.del):not(.equal)"); //Todos os botões, exceto DELETE e EQUAL
+const operationButtons = document.querySelectorAll(".rowCalculations button");
+const deleteButton = document.querySelector(".numbers-grid .del");
+const equalButton = document.querySelector(".numbers-grid .equal");
+
+
+function appendToInput(char) {
+  inputText.value += char;
+}
+
+
+// Usuário consegue selecionar os números
+numberButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    appendToInput(button.textContent); //Ao clicar em qualquer botão de número, é selecionado o texto do botão (o valor)
+  });
+});
+
+
+// Usuário escolhe a operação
+operationButtons.forEach(button => {
+  let op;
+  switch (button.id) {
+    case "btnSoma": op = "+"; break; // A variável "op" tem seu valor atribuído de acordo com o ID do botão
+    case "btnSubtracao": op = "-"; break;
+    case "btnMultiplicacao": op = "*"; break; 
+    case "btnDivisao": op = "/"; break;       
+  }
+
+  button.addEventListener("click", () => {
+    appendToInput(op);
+  });
+});
+
+
+// Função do botão Delete
+deleteButton.addEventListener("click", () => {
+  inputText.value = "";
+  resultText.value = "";
+});
+
+
+// Função do botão Equal
+equalButton.addEventListener("click", () => {
+  try {
+    // Calcula usando eval
+    const resultado = eval(inputText.value);
+    resultText.value = resultado;
+  } catch (error) {
+    resultText.value = "Erro";
+  }
+});
